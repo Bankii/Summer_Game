04f627177289e48b35b682bf3069ab428dd5b5d6
Bankii <gmartinezribot@gmail.com>
2017-01-07 11:50:48 -0300

    Reset

--- Assets/Scripts/game/CPlatform.cs
+++ Assets/Scripts/game/CPlatform.cs
@@ -17,7 +17,7 @@ public class CPlatform : CGameObject {
     private const int PLATFORM_WIDTH = 85;
 
     private GameObject _platformType;
-    private Sprite[] _platformSprite;
+    public Sprite[] _platformSprite;
     private SpriteRenderer _spriteRenderer;
     //private SpriteRenderer _spriteRendererGreen;
     //private SpriteRenderer _spriteRendererRed;
@@ -40,27 +40,27 @@ public class CPlatform : CGameObject {
             //_spriteRenderer.sprite = el sprite que quieras.
             setName("Platform_Green");
         }
-        else if (getType() == PLATFORM_RED)
-        {
-            //prefab RED
-            Instantiate(_platformType, new Vector3(1200 + PLATFORM_WIDTH, -600), Quaternion.identity);
-            //_spriteRendererRed = _platformType.GetComponent<SpriteRenderer>();
-            setName("Platform_Red");
-        }
-        else if (getType() == PLATFORM_YELLOW)
-        {
-            //prefab YELLOW
-            Instantiate(_platformType, new Vector3(1200 + PLATFORM_WIDTH * 2, -600), Quaternion.identity);
-            //_spriteRendererYellow = _platformType.GetComponent<SpriteRenderer>();
-            setName("Platform_Yellow");
-        }
-        else if (getType() == PLATFORM_BLUE)
-        {
-            //prefab BLUE
-            Instantiate(_platformType, new Vector3(1200 + PLATFORM_WIDTH * 3, -600), Quaternion.identity);
-            //_spriteRendererBlue = _platformType.GetComponent<SpriteRenderer>();
-            setName("Platform_Blue");
-        }
+        //else if (getType() == PLATFORM_RED)
+        //{
+        //    //prefab RED
+        //    Instantiate(_platformType, new Vector3(1200 + PLATFORM_WIDTH, -600), Quaternion.identity);
+        //    _spriteRendererRed = _platformType.GetComponent<SpriteRenderer>();
+        //    setName("Platform_Red");
+        //}
+        //else if (getType() == PLATFORM_YELLOW)
+        //{
+        //    //prefab YELLOW
+        //    Instantiate(_platformType, new Vector3(1200 + PLATFORM_WIDTH * 2, -600), Quaternion.identity);
+        //    _spriteRendererYellow = _platformType.GetComponent<SpriteRenderer>();
+        //    setName("Platform_Yellow");
+        //}
+        //else if (getType() == PLATFORM_BLUE)
+        //{
+        //    //prefab BLUE
+        //    Instantiate(_platformType, new Vector3(1200 + PLATFORM_WIDTH * 3, -600), Quaternion.identity);
+        //    _spriteRendererBlue = _platformType.GetComponent<SpriteRenderer>();
+        //    setName("Platform_Blue");
+        //}
 
 
     }
@@ -87,58 +87,59 @@ public class CPlatform : CGameObject {
 
     void Update()
     {
+
         apiUpdate();
     }
     
 
-    public override void apiUpdate()
-    {
-        base.apiUpdate();
-        Debug.Log("CPlatform apiUpdate");
-        if (getState() == STATE_OFF)
-        {
-            Debug.Log(getState());
-            if (getType() == PLATFORM_GREEN)
-            {
-                Debug.Log("State OFF Renderer Green " + _platformSprite[0]);
-                _spriteRenderer.sprite = _platformSprite[0];
+    //public override void apiUpdate()
+    //{
+    //    base.apiUpdate();
+    //    Debug.Log("CPlatform apiUpdate");
+    //    if (getState() == STATE_OFF)
+    //    {
+    //        Debug.Log(getState());
+    //        if (getType() == PLATFORM_GREEN)
+    //        {
+    //            Debug.Log("State OFF Renderer Green " + _platformSprite[0]);
+    //            _spriteRendererGreen.sprite = _platformSprite[0];
+                
+    //        }
+    //        else if (getType() == PLATFORM_RED)
+    //        {
+    //            _spriteRendererRed.sprite = _platformSprite[2];
+    //            Debug.Log("State OFF Renderer Red " + _spriteRendererRed);
+    //        }
+    //        else if (getType() == PLATFORM_YELLOW)
+    //        {
+    //            _spriteRendererYellow.sprite = _platformSprite[4];
+    //        }
+    //        else if (getType() == PLATFORM_BLUE)
+    //        {
+    //            _spriteRendererBlue.sprite = _platformSprite[6];
+    //        }
+    //    }
+    //    if (getState() == STATE_ON)
+    //    {
+    //        Debug.Log(getState());
+    //        if (getType() == PLATFORM_GREEN)
+    //        {                
+    //            Debug.Log("State ON Renderer Green " + _platformSprite[1]);
+    //            _spriteRendererGreen.sprite = _platformSprite[1];
+    //        }
+    //        else if (getType() == PLATFORM_RED)
+    //        {
+    //            _spriteRendererRed.sprite = _platformSprite[3];
+    //        }
+    //        else if (getType() == PLATFORM_YELLOW)
+    //        {
+    //            _spriteRendererYellow.sprite = _platformSprite[5];
+    //        }
+    //        else if (getType() == PLATFORM_BLUE)
+    //        {
+    //            _spriteRendererBlue.sprite = _platformSprite[7];
+    //        }
+    //    }
 
-            }
-            else if (getType() == PLATFORM_RED)
-            {
-                _spriteRenderer.sprite = _platformSprite[2];
-                Debug.Log("State OFF Renderer Red " + _spriteRenderer);
-            }
-            else if (getType() == PLATFORM_YELLOW)
-            {
-                _spriteRenderer.sprite = _platformSprite[4];
-            }
-            else if (getType() == PLATFORM_BLUE)
-            {
-                _spriteRenderer.sprite = _platformSprite[6];
-            }
-        }
-        if (getState() == STATE_ON)
-        {
-            Debug.Log(getState());
-            if (getType() == PLATFORM_GREEN)
-            {
-                Debug.Log("State ON Renderer Green " + _platformSprite[1]);
-                _spriteRenderer.sprite = _platformSprite[1];
-            }
-            else if (getType() == PLATFORM_RED)
-            {
-                _spriteRenderer.sprite = _platformSprite[3];
-            }
-            else if (getType() == PLATFORM_YELLOW)
-            {
-                _spriteRenderer.sprite = _platformSprite[5];
-            }
-            else if (getType() == PLATFORM_BLUE)
-            {
-                _spriteRenderer.sprite = _platformSprite[7];
-            }
-        }
-
-    }
+    //}
 }
