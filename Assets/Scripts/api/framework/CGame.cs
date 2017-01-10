f8c784dc71d72707711e4d4a1e28327d65b6cc0c
Bankii <gmartinezribot@gmail.com>
2017-01-10 18:17:49 -0300

    The Simon sequences are showing now.

--- Assets/Scripts/api/framework/CGame.cs
+++ Assets/Scripts/api/framework/CGame.cs
@@ -10,11 +10,13 @@ public class CGame : MonoBehaviour
 	private CCamera mCamera;
 	private CPlayer mPlayer;
 
-    private List<int> _simonSecuence;
+    private List<int> _simonSequence;
     private int _randomNum;
     private int _difficulty;
+    private int _platformCount;
     private bool _isSolved;
-    private bool _isShowing;
+    private bool _isShowed;
+    private bool _firstTimeShowSequence;
     private bool _isGameOver; //testing
 
     public GameObject _platformGreen;
@@ -65,11 +67,13 @@ public class CGame : MonoBehaviour
 	void Start () 
 	{
         _difficulty = 0;
-        _simonSecuence = new List<int>();
+        _simonSequence = new List<int>();
         _isSolved = true;
-        _isShowing = false;
-        _isGameOver = false;  
-         
+        _isShowed = false;
+        _isGameOver = false;
+        _firstTimeShowSequence = true;
+        _platformCount = 0;
+
         //Instantiating Platforms       
         _platformGreen = Instantiate(_platformGreen, new Vector3(1200, -600), Quaternion.identity);
         _platformGreenScript =  _platformGreen.GetComponent<CPlatform>();
@@ -105,155 +109,28 @@ public class CGame : MonoBehaviour
 		CMouse.update ();
 		CKeyboard.update ();
 
-        //If the previous secuence was solved or it's the first, build and show Simon Secuence
+        //If the previous sequence was solved or it's the first, build and show Simon Sequence
         if (_isSolved && !_isGameOver)
-        {            
-            for (int i = 0; i <= _difficulty; i++)
-            {
-                _randomNum = CMath.randomIntBetween(0, 3);
-                //_randomNum = 0;
-
-                _simonSecuence.Add(_randomNum);               
-
-                //Debug.Log for testing
-                switch (_randomNum)
-                {
-                    case 0:
-                        //Green
-                        Debug.Log("Secuence: Green");
-
-                        break;
-                    case 1:
-                        //Red
-                        Debug.Log("Secuence: Red");
-
-                        break;
-                    case 2:
-                        //Yellow
-                        Debug.Log("Secuence: Yellow");
-
-                        break;
-                    case 3:
-                        //Blue
-                        Debug.Log("Secuence: Blue");
-
-                        break;
-                    default:
-                        break;
-                }
-                
-            }                    
-
-            _isSolved = false;
-
-            //Show secuence in platforms
-            foreach (int platform in _simonSecuence)
-            {
-                Debug.Log("Showing: " + platform);
-                if (platform == PLATFORM_GREEN)
-                {
-                    _platformGreenScript.setState(STATE_PLATFORM_ON);
-                    _isShowing = true;
-                    return;
-                }
-                if (platform == PLATFORM_RED)
-                {
-                    _platformRedScript.setState(STATE_PLATFORM_ON);
-                    _isShowing = true;
-                    return;
-                }
-                if (platform == PLATFORM_YELLOW)
-                {
-                    _platformYellowScript.setState(STATE_PLATFORM_ON);
-                    _isShowing = true;
-                    return;
-                }
-                if (platform == PLATFORM_BLUE)
-                {
-                    _platformBlueScript.setState(STATE_PLATFORM_ON);
-                    _isShowing = true;
-                    return;
-                }
-
-                while (_isShowing)
-                {
-                    if (_platformGreenScript.getState() == STATE_PLATFORM_OFF && _platformRedScript.getState() == STATE_PLATFORM_OFF &&
-                        _platformYellowScript.getState() == STATE_PLATFORM_OFF && _platformBlueScript.getState() == STATE_PLATFORM_OFF)
-                    {
-                        _isShowing = false;
-                    }
-                }
-            }
+        {
+            buildSimonSequence();
         }
         else if (!_isSolved && !_isGameOver)
         {
-            //Player Input Secuence
-            //Replace CKeyboard.pressed with collision detection
-            if (CKeyboard.pressed(KeyCode.G))
-            {
-                Debug.Log("G");
-                if (_simonSecuence[0] == 0)
-                {
-                    Debug.Log("Green: Correct");
-                    _simonSecuence.RemoveAt(0);
-                }
-                else
-                {
-                    _isGameOver = true;
-                }
-            }
-            else if (CKeyboard.pressed(KeyCode.R))
-            {
-                Debug.Log("R");
-                if (_simonSecuence[0] == 1)
-                {
-                    Debug.Log("Red: Correct");
-                    _simonSecuence.RemoveAt(0);
-                }
-                else
-                {
-                    _isGameOver = true;
-                }
-            }
-            else if (CKeyboard.pressed(KeyCode.Y))
-            {
-                Debug.Log("Y");
-                if (_simonSecuence[0] == 2)
-                {
-                    Debug.Log("Yellow: Correct");
-                    _simonSecuence.RemoveAt(0);
-                }
-                else
-                {
-                    _isGameOver = true;
-                }
-            }
-            else if (CKeyboard.pressed(KeyCode.B))
-            {
-                Debug.Log("B");
-                if (_simonSecuence[0] == 3)
-                {
-                    Debug.Log("Blue: Correct");
-                    _simonSecuence.RemoveAt(0);
-                }
-                else
-                {
-                    _isGameOver = true;
-                }
-            }
-
-            if (_simonSecuence.Count == 0)
-            {
-                _isSolved = true;
-                _difficulty = _difficulty + CGameConstants.DIFFICULTY_INCREMENT;
-                Debug.Log("You WIN, next secuence...");
-            }
+            //Player Input Sequence
+            playerInput();
+            checkWin();
         }
         else if (_isGameOver)
         {
             Debug.Log("You LOSE");
         }
-         
+
+        //If it isn't shown, show the Simon Sequence built.
+        if (!_isShowed)
+        {
+            showSimonSequence();
+        }
+
     }
 
     private void render()
@@ -309,5 +186,173 @@ public class CGame : MonoBehaviour
 	{
 		return mPlayer;
 	}
-      
+
+    private void buildSimonSequence()
+    {
+        //Reseting variables for showSimonSequence()
+        _platformCount = 0;
+        _firstTimeShowSequence = true;
+
+        for (int i = 0; i <= _difficulty; i++)
+        {
+            _randomNum = CMath.randomIntBetween(0, 3);
+            //_randomNum = 0;
+
+            _simonSequence.Add(_randomNum);
+
+            //Debug.Log for testing
+            switch (_randomNum)
+            {
+                case 0:
+                    //Green
+                    Debug.Log("Sequence: Green");
+
+                    break;
+                case 1:
+                    //Red
+                    Debug.Log("Sequence: Red");
+
+                    break;
+                case 2:
+                    //Yellow
+                    Debug.Log("Sequence: Yellow");
+
+                    break;
+                case 3:
+                    //Blue
+                    Debug.Log("Sequence: Blue");
+
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+        _isSolved = false;
+        _isShowed = false;
+
+    }
+
+    private void showSimonSequence()
+    {
+        if (_platformCount < _simonSequence.Count)
+        {
+            if (!_firstTimeShowSequence)
+            {
+                if (_platformGreenScript.getState() == STATE_PLATFORM_OFF && _platformRedScript.getState() == STATE_PLATFORM_OFF &&
+                _platformYellowScript.getState() == STATE_PLATFORM_OFF && _platformBlueScript.getState() == STATE_PLATFORM_OFF)
+                {
+                    _platformCount++;
+                }
+            }
+            
+            _firstTimeShowSequence = false;
+            
+            if (_simonSequence[_platformCount] == PLATFORM_GREEN)
+            {
+                if (_platformGreenScript.getState() == STATE_PLATFORM_OFF)
+                {
+                    Debug.Log("Green ON");
+                    _platformGreenScript.setState(STATE_PLATFORM_ON);
+                }
+
+            }
+            if (_simonSequence[_platformCount] == PLATFORM_RED)
+            {
+                if (_platformRedScript.getState() == STATE_PLATFORM_OFF)
+                {                    
+                    _platformRedScript.setState(STATE_PLATFORM_ON);
+                }
+            }
+            if (_simonSequence[_platformCount] == PLATFORM_YELLOW)
+            {
+                if (_platformYellowScript.getState() == STATE_PLATFORM_OFF)
+                {
+                    _platformYellowScript.setState(STATE_PLATFORM_ON);
+                }
+            }
+            if (_simonSequence[_platformCount] == PLATFORM_BLUE)
+            {
+                if (_platformBlueScript.getState() == STATE_PLATFORM_OFF)
+                {
+                    _platformBlueScript.setState(STATE_PLATFORM_ON);
+                }
+            }
+
+            //_platformCount++;            
+
+        }
+        else
+        {
+            _isShowed = true;
+        }
+    }
+
+    private void playerInput()
+    {
+        //Replace CKeyboard.pressed with collision detection
+        if (CKeyboard.pressed(KeyCode.G))
+        {
+            Debug.Log("G");
+            if (_simonSequence[0] == 0)
+            {
+                Debug.Log("Green: Correct");
+                _simonSequence.RemoveAt(0);
+            }
+            else
+            {
+                _isGameOver = true;
+            }
+        }
+        else if (CKeyboard.pressed(KeyCode.R))
+        {
+            Debug.Log("R");
+            if (_simonSequence[0] == 1)
+            {
+                Debug.Log("Red: Correct");
+                _simonSequence.RemoveAt(0);
+            }
+            else
+            {
+                _isGameOver = true;
+            }
+        }
+        else if (CKeyboard.pressed(KeyCode.Y))
+        {
+            Debug.Log("Y");
+            if (_simonSequence[0] == 2)
+            {
+                Debug.Log("Yellow: Correct");
+                _simonSequence.RemoveAt(0);
+            }
+            else
+            {
+                _isGameOver = true;
+            }
+        }
+        else if (CKeyboard.pressed(KeyCode.B))
+        {
+            Debug.Log("B");
+            if (_simonSequence[0] == 3)
+            {
+                Debug.Log("Blue: Correct");
+                _simonSequence.RemoveAt(0);
+            }
+            else
+            {
+                _isGameOver = true;
+            }
+        }
+    }
+
+    private void checkWin()
+    {
+        if (_simonSequence.Count == 0)
+        {
+            _isSolved = true;
+            _difficulty = _difficulty + CGameConstants.DIFFICULTY_INCREMENT;
+            Debug.Log("You WIN, next sequence...");
+        }
+    }
 }
